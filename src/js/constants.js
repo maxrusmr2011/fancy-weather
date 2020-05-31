c82358f9f596741e9e2853670e4570930526fd17
maxrusmr2011@gmail.com <maxrusmr2011@gmail.com>
2020-05-31 19:03:56 +0300

    feat: add constants

--- src/js/constants.js
+++ src/js/constants.js
@@ -2,23 +2,43 @@ const ipinfoApi = {
   url: 'https://ipinfo.io/json',
   token: '720b5d732dcf97',
 };
+const geocodeApi = {
+  url: 'https://api.opencagedata.com/geocode/v1/json',
+  token: 'b7d85b0ed426411cbaba7a2d62d575dc',
+};
 const weatherApi = {
   url: 'https://api.openweathermap.org/data/2.5/forecast',
   token: '50d5c38b01dfe0b10a1a4ff714cdd3d1',
 };
 const photoApi = {
-  url: 'https://api.unsplash.com/photos/random',
+  url: '_https://api.unsplash.com/photos/random',
   token: 'ToCg_dlprnchoDQzAFEdnUjGWwARHG_yYld-JuEYFqk',
 };
-const mapApi = {
-  url: 'https://maps.googleapis.com/maps/api/js',
-  token: 'AIzaSyDn_gIbGSdCoGt7J26p_capXd8MKnDzvYM',
+const translateApi = {
+  url: 'https://translate.yandex.net/api/v1.5/tr.json/translate',
+  token: 'trnsl.1.1.20200422T111801Z.e0d39b73a715a384.a2a2b921649e23f209f29f7200aebd93340f4270',
 };
+// const mapApiGoogle = {
+//   url: 'https://maps.googleapis.com/maps/api/js',
+//   token: 'AIzaSyDn_gIbGSdCoGt7J26p_capXd8MKnDzvYM',
+// };
 
+const mapApiMapboxToken = 'pk.eyJ1IjoibWF4cnVzbXIyMDExIiwiYSI6ImNrYTQwcnFmNzA5ZjYzZm12ZXQzbXFhN2QifQ.uSxrGMLSQh-0N0QECC_3iQ';
+const images = {
+  refresh: './src/assets/img/refresh.svg',
+  mic: './src/assets/img/microphone.png',
+  patch: './src/assets/img/patchPhoto.png',
+  speak: './src/assets/img/sound.png',
+};
+const mainLang = 'en';
 
 export {
   ipinfoApi,
   weatherApi,
   photoApi,
-  mapApi,
+  mapApiMapboxToken,
+  geocodeApi,
+  translateApi,
+  images,
+  mainLang,
 };
---
+++ src/js/weatherIcons.js
@@ -0,0 +1,77 @@
+export default [
+  {
+    from: 299,
+    src_day: './src/assets/img/weather/thunderstorm.svg',
+    src_night: './src/assets/img/weather/thunderstorm.svg',
+  },
+  {
+    from: 300,
+    src_day: './src/assets/img/weather/patchy-drizzle-day.svg',
+    src_night: './src/assets/img/weather/patchy-drizzle-night.svg',
+  },
+  {
+    from: 399,
+    src_day: './src/assets/img/weather/drizzle.svg',
+    src_night: './src/assets/img/weather/drizzle.svg',
+  },
+  {
+    from: 501,
+    src_day: './src/assets/img/weather/patchy-rain-day.svg',
+    src_night: './src/assets/img/weather/patchy-rain-night.svg',
+  },
+  {
+    from: 599,
+    src_day: './src/assets/img/weather/rain.svg',
+    src_night: './src/assets/img/weather/rain.svg',
+  },
+  {
+    from: 600,
+    src_day: './src/assets/img/weather/patchy-snow-day.svg',
+    src_night: './src/assets/img/weather/patchy-snow-night.svg',
+  },
+  {
+    from: 602,
+    src_day: './src/assets/img/weather/snow.svg',
+    src_night: './src/assets/img/weather/snow.svg',
+  },
+  {
+    from: 612,
+    src_day: './src/assets/img/weather/patchy-sleet-day.svg',
+    src_night: './src/assets/img/weather/patchy-sleet-night.svg',
+  },
+  {
+    from: 699,
+    src_day: './src/assets/img/weather/sleet.svg',
+    src_night: './src/assets/img/weather/sleet.svg',
+  },
+  {
+    from: 780,
+    src_day: './src/assets/img/weather/fog.svg',
+    src_night: './src/assets/img/weather/fog.svg',
+  },
+  {
+    from: 799,
+    src_day: './src/assets/img/weather/tornado.svg',
+    src_night: './src/assets/img/weather/tornado.svg',
+  },
+  {
+    from: 800,
+    src_day: './src/assets/img/weather/sunny.svg',
+    src_night: './src/assets/img/weather/clear-night.svg',
+  },
+  {
+    from: 801,
+    src_day: './src/assets/img/weather/partly-cloudy-day.svg',
+    src_night: './src/assets/img/weather/partly-cloudy-night.svg',
+  },
+  {
+    from: 802,
+    src_day: './src/assets/img/weather/few-clouds.svg',
+    src_night: './src/assets/img/weather/few-clouds.svg',
+  },
+  {
+    from: 899,
+    src_day: './src/assets/img/weather/dark-clouds.svg',
+    src_night: './src/assets/img/weather/dark-clouds.svg',
+  },
+];
